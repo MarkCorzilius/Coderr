8e69668db273adf21491f7783af31c74151d82df
Mark Corzilius <marccorzprivate@gmail.com>
2026-07-24 18:23:25 +0200

    feat(base): add base_info GET endpoint with integration tests

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from core import settings
+from core.views import BaseInfoAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('api/', include('offers_app.api.urls')),
     path('api/', include('orders_app.api.urls')),
     path('api/', include('reviews_app.api.urls')),
+    path('api/base-info/', BaseInfoAPIView.as_view(), name='base-info')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

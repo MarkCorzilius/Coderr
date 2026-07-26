e8aece04431e80ae1897d96565dc63e0c6ef1303
Mark Corzilius <marccorzprivate@gmail.com>
2026-07-26 12:29:36 +0200

    fix: remove duplicate serializer definitions in offers_app

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -93,12 +93,12 @@ class OfferCreateUpdateSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Update offer fields and each specified nested detail."""
-    
+
         details_data = validated_data.pop("details", [])
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
-    
+
         for detail_data in details_data:
             detail_id = detail_data.get("id")
             if detail_id:
@@ -110,12 +110,12 @@ class OfferCreateUpdateSerializer(serializers.ModelSerializer):
                         {"details": "Each detail update requires an id or offer_type."}
                     )
                 detail = OfferDetail.objects.get(offer=instance, offer_type=offer_type)
-    
+
             for attr, value in detail_data.items():
                 if attr not in ("id", "offer_type"):
                     setattr(detail, attr, value)
             detail.save()
-    
+
         return instance
 
 
@@ -141,128 +141,3 @@ class OfferRetrieveSerializer(serializers.ModelSerializer):
             "min_delivery_time",
         ]
         read_only_fields = ["id", "created_at", "updated_at", "min_price", "min_delivery_time"]
-
-
-class OfferDetailHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = OfferDetail
-        fields = ['id', 'url']
-        extra_kwargs = {
-            'url': {'read_only': True}
-            }
-
-
-class OfferDetailSerializer(serializers.ModelSerializer):
-
-    id = serializers.IntegerField(required=False)
-
-    class Meta:
-        model = OfferDetail
-        fields = [ 
-            "id",
-            "title", 
-            "revisions", 
-            "delivery_time_in_days", 
-            "price", 
-            "features", 
-            "offer_type",
-            ]
-        
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("Price cannot be negative.")
-        return value
-
-
-class OfferListSerializer(serializers.ModelSerializer):
-
-    details = OfferDetailHyperlinkedSerializer(many=True)
-    user_details = UserDetailsSerializer(source='user', read_only=True)
-    min_price = serializers.IntegerField(read_only=True)
-    min_delivery_time = serializers.IntegerField(read_only=True)
-    
-    class Meta:
-        model = Offer
-        fields = [
-            "id", 
-            "user", 
-            "title", 
-            "image", 
-            "description", 
-            "created_at", 
-            "updated_at", 
-            "details",
-            "min_price", 
-            "min_delivery_time",
-            'user_details'
-            ]
-        
-        read_only_fields = ['id', 'created_at', 'updated_at', 'min_price', 'min_delivery_time']
-        
-
-class OfferCreateUpdateSerializer(serializers.ModelSerializer):
-
-    details = OfferDetailSerializer(many=True)
-
-    class Meta:
-        model = Offer
-        fields = [
-            "id",
-            "title",
-            "image",
-            "description",
-            "details",
-            ]
-        
-        read_only_fields = ['id']
-        
-    def create(self, validated_data):
-        details_data = validated_data.pop('details', [])
-        offer = Offer.objects.create(**validated_data)
-        for detail in details_data:
-            OfferDetail.objects.create(offer=offer, **detail)
-        return offer
-    
-    def update(self, instance, validated_data):
-        details_data = validated_data.pop("details", [])
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-
-        for detail_data in details_data:
-            detail_id = detail_data.get("id")
-            if not detail_id:
-                raise serializers.ValidationError({"details": "Each detail update requires an id."})
-            detail = OfferDetail.objects.get(id=detail_id, offer=instance)
-            for attr, value in detail_data.items():
-                if attr != "id":
-                    setattr(detail, attr, value)
-            detail.save()   
-
-        return instance
-        
-
-class OfferRetrieveSerializer(serializers.ModelSerializer):
-
-    details = OfferDetailHyperlinkedSerializer(many=True, read_only=True)
-    min_price = serializers.IntegerField(read_only=True)
-    min_delivery_time = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = Offer
-        fields = [
-            "id",
-            "user",
-            "title",
-            "image",
-            "description",
-            "created_at",
-            "updated_at",
-            "details",
-            "min_price",
-            "min_delivery_time",
-            ]
-        
-        read_only_fields = ['id', 'created_at', 'updated_at', 'min_price', 'min_delivery_time']

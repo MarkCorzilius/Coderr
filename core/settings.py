fc92000a6601c1ecd784df511b6857692d119571
Mark Corzilius <marccorzprivate@gmail.com>
2026-09-16 16:21:04 +0200

    fix(settings): fixed cors_allowed_origins to fit for prod

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,8 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 CORS_ALLOWED_ORIGINS = [
+    "https://markcorzilius.com",
+    "https://www.markcorzilius.com",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
 ]
